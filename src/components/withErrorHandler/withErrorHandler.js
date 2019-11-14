34b70dae81f484adbd5dbd05d6e53211733cd6a3
Devon Deason <devon.d.deason@gmail.com>
2019-11-13 18:34:10 -0700

    whatever

--- src/components/withErrorHandler/withErrorHandler.js
+++ src/components/withErrorHandler/withErrorHandler.js
@@ -10,17 +10,22 @@ const withErrorHandler = (WrappedComponent, axios) => {
     };
 
     componentWillMount() {
-      axios.interceptors.response.use(req => {
+      this.reqInterceptor = axios.interceptors.response.use(req => {
         this.setState({ error: null });
         return req;
       });
 
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({ error: error });
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
