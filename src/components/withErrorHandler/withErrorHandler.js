d911e16c9c8c41d74c5301313e219554ce7c124a
Devon Deason <devon.d.deason@gmail.com>
2019-11-13 18:10:42 -0700

    not a clue what I did here

---
+++ src/components/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import Modal from "../../components/UI/Modal/Modal";
+
+// This is a little confusing, we've made a higher order function here.
+// We can wrap any export with this function so that we can fire the error modal just about anywhere
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+    componentDidMount() {
+      axios.interceptors.response.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({ error: null });
+    };
+
+    render() {
+      return (
+        <>
+          <Modal
+            show={this.state.error}
+            backdropClick={this.errorConfirmedHandler}
+          >
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />;
+        </>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Modal from "./../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSumary";
 import axios from "../../axios-orders";
 import Spinner from "../../components/UI/Spinner/Spinner";
+import withErrorHandler from "./../../components/withErrorHandler/withErrorHandler";
 
 const INGREDIENT_PRICES = {
   salad: 0.4,
@@ -187,4 +188,4 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
