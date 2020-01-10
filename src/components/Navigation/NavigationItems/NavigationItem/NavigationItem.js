c0fe5401d8d82299ac213f0c428e7e9df2aa00fd
Devon Deason <devon.d.deason@gmail.com>
2020-01-10 14:37:11 -0700

    setup the order route

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,18 +1,20 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
 import classes from "./NavigationItem.module.css";
+
 import PropTypes from "prop-types";
 
 const navigationItem = props => (
   <li className={classes.NavigationItem}>
-    <a href={props.link} className={props.active && classes.active}>
+    <NavLink to={props.link} activeClassName={classes.active} exact>
       {props.children}
-    </a>
+    </NavLink>
   </li>
 );
 
 navigationItem.propTypes = {
-  link: PropTypes.string.isRequired,
-  active: PropTypes.bool
+  link: PropTypes.string.isRequired
 };
 
 export default navigationItem;
